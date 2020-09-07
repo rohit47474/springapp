545ae2eda6f58a87013039a015760df43200ba3a
rohit47474 <67234160+rohit47474@users.noreply.github.com>
2020-09-07 09:38:32 +0530

    client new changes 1

---
+++ DemoProject/src/com/accenture/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.accenture;
+
+import com.infosys.Display;
+
+public class HelloWorld  extends Display{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Display.wish();
+	
+	}
+
+}
---
+++ DemoProject/src/com/infosys/Display.java
@@ -0,0 +1,7 @@
+package com.infosys;
+
+ public  class Display {
+protected static void wish() {
+	System.out.println("Good morning JAVA");
+}
+}
